0d8db56e0978b18e7bdf92d5c6e850ab474bfe55
ela <eesmot@gmail.com>
2014-12-25 09:15:39 +0600

    Initial Work...

---
+++ form.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<table  border="1", style="width:50%">
+<tr>
+    <td>Track Information</td>
+    <td><a href="#">Edit</a></td>
+
+</tr>
+
+  <tr>
+    <td>Preferred Track (First)</td>
+    <td></td>
+
+  </tr>
+  <tr>
+    <td>S.S.C Roll</td>
+    <td></td>
+
+  </tr>
+<tr>
+    <td>S.S.C Roll</td>
+    <td></td>
+
+</tr>
+<tr>
+    <td>S.S.C Board</td>
+    <td></td>
+
+</tr>
+
+
+
+<tr>
+    <td>H.S.C Roll</td>
+    <td></td>
+
+</tr>
+<tr>
+    <td>H.S.C Roll</td>
+    <td></td>
+
+</tr>
+<tr>
+    <td>H.S.C Board</td>
+    <td></td>
+
+</tr>
+<tr>
+    <td>Has Laptop</td>
+    <td></td>
+
+</tr>
+<tr>
+    <td>Preferred Exam Center</td>
+    <td></td>
+
+</tr>
+
+</table>
+
+<br>
+<br>
+<br>
+
+<table  border="1", style="width:50%">
+<tr>
+    <td>Personal Information</td>
+    <td><a href="#"></a></td>
+
+</tr>
+
+<tr>
+    <td>Father's Name
+    </td>
+    <td></td>
+
+</tr>
+<tr>
+    <td>S.S.C Roll</td>
+    <td></td>
+
+</tr>
+<tr>
+    <td>S.S.C Roll</td>
+    <td></td>
+
+</tr>
+<tr>
+    <td>S.S.C Board</td>
+    <td></td>
+
+</tr>
+
+
+
+<tr>
+    <td>H.S.C Roll</td>
+    <td></td>
+
+</tr>
+<tr>
+    <td>H.S.C Roll</td>
+    <td></td>
+
+</tr>
+<tr>
+    <td>H.S.C Board</td>
+    <td></td>
+
+</tr>
+<tr>
+    <td>Has Laptop</td>
+    <td></td>
+
+</tr>
+<tr>
+    <td>Preferred Exam Center</td>
+    <td></td>
+
+</tr>
+
+</table>
+</body>
+</html>
