cc8d34c0686ecad2daf57a928ecf1b652e220b36
dolon <dolon1090@yahoo.com>
2014-12-25 23:16:24 +0600

    Updating create.php

--- personal_info/create.php
+++ personal_info/create.php
@@ -6,13 +6,13 @@
     <input type="text" name="track" /><br>
 
     <label>S.S.C. Roll: </label>
-    <input type="text" name="ssc_roll" /> <br>
+    <input type="number" name="ssc_roll" /> <br>
 
     <label>H.S.C. Roll: </label>
-    <input type="email" name="hsc_roll" ><br>
+    <input type="number" name="hsc_roll" ><br>
 
     <label>Has Laptop: </label>
-    <input type="email" name="laptop" ><br>
+    <input type="text" name="laptop" ><br>
 
     <h1>Personal Information</h1><br>
 
@@ -29,35 +29,11 @@
     <input type="text" name="religion" ><br>
 
     <label>Date Of Birth:</label>
-    <select>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-    </select>
+    <input type="text" value="dob">
 
-
-    <select>
-        <option value="1">Jan</option>
-        <option value="2">Feb</option>
-        <option value="3">Mar</option>
-        <option value="4">Oct</option>
-        <option value="5">Nov</option>
-    </select>
-
-
-    <select>
-        <option value="1">1990</option>
-        <option value="2">1991</option>
-        <option value="3">1992</option>
-        <option value="4">1993</option>
-        <option value="5">1994</option>
-    </select><br>
 
     <label>Gender: </label>
-    <input type="radio" name="sex" value="male" >Male
-    <input type="radio" name="sex" value="female" >Female<br>
+    <input type="text" name="sex">
 
     <label> Nationality </label>
     <input type="text" name="nationality"><br>
