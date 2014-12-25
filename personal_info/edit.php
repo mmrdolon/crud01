9f5e0e88ab9e4b2b38d10dbd47078db173dd5e7e
dolon <dolon1090@yahoo.com>
2014-12-26 02:16:58 +0600

    Uploading edit.php

--- personal_info/edit.php
+++ personal_info/edit.php
@@ -2,7 +2,7 @@
 $ssc_roll = $_GET['ssc_roll'];
 $link = mysqli_connect("localhost",
     "root",
-    "mysql123",
+    "",
     "crud");
 $query = "select * from personal_info WHERE ssc_roll = $ssc_roll";
 $result = mysqli_query($link, $query);
