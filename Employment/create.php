da41f100f0c8309e6272f6538251facc61223cbc
dolon <dolon1090@yahoo.com>
2014-12-27 02:07:21 +0600

    create.php of Employment

---
+++ Employment/create.php
@@ -0,0 +1,30 @@
+<form action="add.php" method="post">
+
+    <h1> Job Information</h1>
+
+    <label>Company Name:</label>
+    <input type="text" name="company_name" /><br>
+
+    <label>Company Business:</label>
+    <input type="text" name="company_business" /><br>
+
+    <label>Address:</label>
+    <input type="text" name="address" /><br>
+
+    <label>Designation</label>
+    <input type="text" name="designation" /><br>
+
+    <label>department:</label>
+    <input type="text" name="department" /><br>
+
+    <label>Duration</label>
+    <input type="text" name="duration" /><br>
+
+    <label>Responsibility:</label>
+    <input type="text" name="responsibility" /><br><br>
+
+
+    <button type="submit">Submit</button>
+
+</form>
+
