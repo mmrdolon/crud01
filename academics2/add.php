b29e3f4d2b389637b30b9680b1063f12730bdcb7
dolon <dolon1090@yahoo.com>
2014-12-26 23:30:39 +0600

    uploading add.php

---
+++ academics2/add.php
@@ -0,0 +1,26 @@
+<?php
+//print_r($_POST);
+
+$Level = $_POST['edu_level'];
+$examTitle  = $_POST['exam_title'];
+$Group  = $_POST['student_group'];
+$Institution  = $_POST['institution'];
+$resultType  = $_POST['result_type'];
+$Result  = $_POST['result'];
+$passingYear  = $_POST['passing_year'];
+$Duration  = $_POST['duration'];
+$Achievement  = $_POST['achievement'];
+
+$link = mysqli_connect("localhost",
+    "root",
+    "",
+    "crud") or die('cannot connect');
+
+$query = "INSERT INTO `crud`.`academic_info2` (`id`,
+`edu_level`,`exam_title`,`student_group`,`institution`,`result_type`,`result`,`passing_year`,`duration`,`achievement`)
+VALUES (NULL ,'$Level', '$examTitle','$Group','$Institution','$resultType','$Result','$passingYear','$Duration','$Achievement')";
+
+//echo $query;
+mysqli_query($link, $query);
+
+header('location:list.php');
